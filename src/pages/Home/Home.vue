583e7c91080aa0e65a44e4dea0a5607e92d1bb71
Evan Moon <bboydart91@gmail.com>
2017-12-22 23:38:06 +0900

    change Home Component to TypeScript

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container-fluid">
     <h1>{{ msg }}</h1>
+    <button @click="updateMsg">test</button>
 
     <!-- MODAL -->
     <signup-modal ref="signupModal"></signup-modal>
@@ -12,7 +13,36 @@
 @import './Home';
 </style>
 
-<script>
-import Home from './Home';
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import SignupModal from '@/components/modals/SignupModal.vue';
+
+@Component({
+    name: 'Home',
+    components: {
+        SignupModal,
+    },
+})
+class Home extends Vue {
+    $refs: {
+        signupModal: any;
+    }
+
+    msg: string;
+
+    constructor () {
+        super();
+
+        this.msg = 'Hello, World';
+    }
+
+    hideModal () {
+        this.$refs.signupModal.hide();
+    }
+
+    updateMsg () {
+        this.msg = 'After Updated';
+    }
+}
 export default Home;
 </script>
