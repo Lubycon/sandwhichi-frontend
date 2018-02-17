5ed4539fc69f54467f6e1f77975682743efeb6f2
Evan Moon <bboydart91@gmail.com>
2018-02-17 16:49:36 +0900

    Add GA_ID to env.constants.js

--- src/constants/env.constant.ts
+++ src/constants/env.constant.ts
@@ -4,33 +4,40 @@ interface ENV_CONFIG {
     API_BASE_URL: string;
     S3_BASE_URL: string;
     G_RECAPTCHA_KEY: string;
+    GA_ID: string;
 }
 
 function getEnv (env: string): ENV_CONFIG {
     let apiBaseUrl: string = '';
     let s3BaseUrl: string = '';
     let gReCaptchaKey: string = '';
+    let gaId: string = '';
 
     if (env === 'production') {
         apiBaseUrl = 'https://api.pixelstairs.com/v1';
         s3BaseUrl = 'https://s3.ap-northeast-2.amazonaws.com/pixelstairs';
         gReCaptchaKey = '6Lcdf0YUAAAAAICBNxMnocAXgye0dAaosWr1mnbw';
+        gaId = 'UA-112072597-1';
+        
     }
     else if (env === 'development') {
         apiBaseUrl = 'https://dev.api.pixelstairs.com/v1';
         s3BaseUrl = 'https://s3.ap-northeast-2.amazonaws.com/dev.pixelstairs';
         gReCaptchaKey = '6Lcdf0YUAAAAAICBNxMnocAXgye0dAaosWr1mnbw';
+        gaId = 'UA-112072597-1';
     }
     else if (env === 'local' || env === 'test') {
         apiBaseUrl = 'http://local.api.pixelstairs.com/v1';
         s3BaseUrl = 'https://s3.ap-northeast-2.amazonaws.com/dev.pixelstairs';
         gReCaptchaKey = '6Lcdf0YUAAAAAICBNxMnocAXgye0dAaosWr1mnbw';
+        gaId = 'UA-112072597-1';
     }
 
     return {
         API_BASE_URL: apiBaseUrl,
         S3_BASE_URL: s3BaseUrl,
         G_RECAPTCHA_KEY: gReCaptchaKey,
+        GA_ID: gaId,
     };
 }
 
@@ -38,4 +45,5 @@ export const {
     API_BASE_URL,
     S3_BASE_URL,
     G_RECAPTCHA_KEY,
+    GA_ID,
 } = getEnv(env);
