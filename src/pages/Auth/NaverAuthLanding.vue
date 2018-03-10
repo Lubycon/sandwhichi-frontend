28b2c3bc76ea22c842d538098d871324a5645905
Evan Moon <bboydart91@gmail.com>
2018-03-10 18:48:05 +0900

    SDW-120 Add getMyInfo method to NaverAuth

--- src/pages/Auth/NaverAuthLanding.vue
+++ src/pages/Auth/NaverAuthLanding.vue
@@ -14,8 +14,11 @@
 /**
  * @class NaverAuthLanding
  * @extends Vue
+ * @member { any } $naverAuth from VuePlugin
  * @member { string } code Naver에서 리턴해준 인증 코드
  * @member { string } state Sandwhichi에서 발급했던 상태 코드
+ * @member { string } error Naver에서 리턴해준 에러 코드
+ * @member { string } errorDescription
  * @desc 해당 페이지는 사용자가 네이버 계정 인증을 진행한 후에 리다이렉트 되는 페이지이다.
  * code와 state를 sandwhichi api서버로 전송해 2차 검증을 실시하고,
  * 회원가입 여부에 따라 로그인을 시키거나 회원가입 페이지로 랜딩시킨다
@@ -26,12 +29,17 @@ import { Vue, Component } from 'vue-property-decorator';
     name: 'AuthNaverLanding',
 })
 class AuthNaverLanding extends Vue {
+    $naverAuth: any;
     code: string = '';
     state: string = '';
+    error: string = '';
+    errorDescription: string = '';
 
     created () {
         this.code = this.$route.query.code;
         this.state = this.$route.query.state;
+        this.error = this.$route.query.error;
+        this.errorDescription = this.$route.query.errorDescription;
     }
 }
 export default AuthNaverLanding;
--- src/plugin/NaverAuth.ts
+++ src/plugin/NaverAuth.ts
@@ -1,13 +1,18 @@
 /**
  * @class NaverAuth
- * @member { string } authUrl
+ * @member { string } authAPI
+ * @member { string } myInfoAPI
  * @member { config } Object
  * @desc 네이버 인증은 RESTful API로 인증 받는 방식이 아닌,
  * 직접 해당 url로 이동하거나 새로운 창을 띄워서 인증을 진행하고 이후 redirect_uri값에 보냈던 uri로 사용자를 리턴시키는 방식이다.
  */
+import Q from 'q';
+import axios from 'axios';
 
 class NaverAuth {
-    authUrl: string = 'https://nid.naver.com/oauth2.0/authorize';
+    authAPI: string = 'https://nid.naver.com/oauth2.0/authorize';
+    myInfoAPI: string = 'https://openapi.naver.com/v1/nid/me';
+
     config: Object = {};
 
     static install (Vue, config) {
@@ -34,9 +39,9 @@ class NaverAuth {
         const clientID: string = (<any>this.config).clientId;
         const redirectURI: string = `${location.origin}${to}`;
         const responseType: string = 'code';
-        const state: string = 'naverauth-sandwhichi';
+        const state: string = encodeURI('naverauth-sandwhichi');
         const locale = 'ko';
-        const uri: string = `${this.authUrl}?client_id=${clientID}&response_type=${responseType}&redirect_uri=${redirectURI}&state=${state}&locale=${locale}`;
+        const uri: string = `${this.authAPI}?client_id=${clientID}&response_type=${responseType}&redirect_uri=${redirectURI}&state=${state}&locale=${locale}`;
         
         if (popup) {
             window.open(uri);
@@ -44,6 +49,22 @@ class NaverAuth {
         else {
             location.href = uri;
         }
+    }
+
+    async getMyInfo (token: string): Promise<any> {
+        const defer = Q.defer();
+        try {
+            const response = await axios.get(this.myInfoAPI, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            defer.resolve(response);
+        }
+        catch (e) {
+            defer.reject(e);
+        }
+        return defer.promise;
     }
 }
 
