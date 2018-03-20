548f6d99afa4aa8fbb6efc8db1681d54816fa474
martinyounghoonkim <hoons0131@gmail.com>
2018-03-20 22:55:42 +0900

    Add ProgressBar component

---
+++ src/components/utils/ProgressBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="progress-bar">
+        <div :style="{ 'width': progressWidth }">
+            {{ progressWidth }} 작성되었습니다.
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .progress-bar{
+        height: 6px;
+        background-color: #f5f5f5;
+        div{
+            overflow: hidden;
+            height: 100%;
+            background-color: #52bad5;
+            text-indent: -10000em;
+        }
+    }
+</style>
+
+<script lang="ts">
+    /**
+     * @class ProgressBar
+     * @extends Vue
+     */
+
+    import { Vue, Component, Prop } from 'vue-property-decorator';
+
+    @Component({
+        name: 'ProgressBar',
+    })
+    class ProgressBar extends Vue {
+        @Prop({ default: 0 }) value: Number;
+
+        get progressWidth (): string {
+            return this.value + '%';
+        }
+    }
+
+    export default ProgressBar;
+</script>
+
