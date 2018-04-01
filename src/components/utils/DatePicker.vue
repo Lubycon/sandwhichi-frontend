1f4682351ecdac6cdbfafb09bac082962d913b30
martinyounghoonkim <hoons0131@gmail.com>
2018-04-01 19:20:42 +0900

    Add util component to using in Register project component

---
+++ src/components/utils/DatePicker.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <vue-datepicker-local v-model="localDate"
+                              :local="local"></vue-datepicker-local>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .progress-bar{
+        height: 6px;
+        background-color: #f5f5f5;
+        div{
+            overflow: hidden;
+            height: 100%;
+            background-color: #52bad5;
+            text-indent: -10000em;
+        }
+    }
+</style>
+
+<script lang="ts">
+    /**
+     * @class DatePicker
+     * @extends Vue
+     */
+
+    import moment, { Moment } from 'moment';
+    import VueDatepickerLocal from 'vue-datepicker-local';
+    import { Vue, Component, Prop } from 'vue-property-decorator';
+
+    interface IdatePickerConfig {
+        dow: number,
+        monthsHead: Array<string>,
+        months: Array<string>,
+        weeks: Array<string>,
+    }
+
+    @Component({
+        name: 'DatePicker',
+        components: {
+            VueDatepickerLocal,
+        },
+    })
+    class DatePicker extends Vue {
+        @Prop() date: Moment;
+
+        get localDate (): string {
+            return this.date.format('YYYY-MM-DD');
+        }
+
+        set localDate (selectedDate) {
+            this.$emit('updateDate', moment(selectedDate));
+        }
+
+        local: IdatePickerConfig = {
+            dow: 0,
+            monthsHead: '1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월'.split('_'),
+            months: '1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월'.split('_'),
+            weeks: '일_월_화_수_목_금_토'.split('_'),
+        }
+    }
+
+    export default DatePicker;
+</script>
+
--- src/pages/Project/Register/Components/ProjectRegisterActionButton.vue
+++ src/pages/Project/Register/Components/ProjectRegisterActionButton.vue
@@ -15,22 +15,34 @@
 </script>
 <style lang="scss" scoped>
     .project-register-action-button-wrapper{
+        overflow: hidden;
         position: fixed;
         left: 0;
         bottom: 0;
         width: 100%;
-        height: 48px;
+        height: 49px;
         z-index: 100;
+        border-top: 1px solid #ccc;
         button{
             width: 100%;
             height: 100%;
             border: 0;
-            background-color: #52bad5;
-            color: #fff;
             font-size: 14px;
             &:focus, &:active{
                 outline: 0;
                 border: 0;
+            }
+            &.is-previous{
+                background-color: #fff;
+                color: #999;
+            }
+            &.is-next{
+                background-color: #52bad5;
+                color: #fff;
+            }
+            &.is-half{
+                float: left;
+                width: 50%;
             }
         }
     }
