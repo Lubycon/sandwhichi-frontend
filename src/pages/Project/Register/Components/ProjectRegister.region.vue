d83620c801e0537fa5dd47c304d6cf0ca585ef13
martinyounghoonkim <hoons0131@gmail.com>
2018-04-04 12:43:54 +0900

    Update Project region component

---
+++ src/pages/Project/Register/Components/ProjectRegister.region.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+<script lang="ts">
+    import { Vue, Component } from 'vue-property-decorator';
+
+    @Component({
+        name: 'ProjectRegisterRegion',
+    })
+
+    class ProjectRegisterRegion extends Vue {
+
+    }
+
+    export default ProjectRegisterRegion;
+</script>
+<style lang="scss" scoped>
+
+</style>
