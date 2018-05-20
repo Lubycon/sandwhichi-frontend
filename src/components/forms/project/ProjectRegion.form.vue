ca6b931f18ddcddb8b51826c1a9e4d373f12c192
martinyounghoonkim <hoons0131@gmail.com>
2018-05-20 10:55:06 +0900

    Merged origin branch

--- src/components/forms/project/ProjectRegion.form.vue
+++ src/components/forms/project/ProjectRegion.form.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>Region</div>
+<b-form-row class="project-form" data-name="project-region">
+    <b-col cols="12">
+        <b-form-group
+            label="지역 1">
+            <b-form-select
+                v-model="region1"
+                :options="regionOptions">
+            </b-form-select>
+        </b-form-group>
+        <b-form-group
+            label="지역 2">
+            <b-form-select
+                v-model="region2"
+                :options="regionOptions">
+            </b-form-select>
+        </b-form-group>
+    </b-col>
+</b-form-row>
 </template>
 <script lang="ts">
     /**
@@ -14,6 +31,12 @@
         name: 'ProjectRegionForm',
     })
     class ProjectRegionForm extends Vue implements FormComponent {
+        region1: string;
+        region2: string;
+        regionOptions: Array<Object> = [
+            { value: 'seoul', text: '서울' },
+            { value: 'gyeonggi', text: '경기도' }
+        ];
         validate (): void {}
     }
 
