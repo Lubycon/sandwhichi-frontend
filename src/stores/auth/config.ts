d35a46a04c07144319e8d8be9305576d0b963db1
Evan Moon <bboydart91@gmail.com>
2018-08-12 15:33:30 +0900

    Update Getters, Actions, Mutations name of Auth Store

---
+++ src/stores/auth/config.ts
@@ -0,0 +1,11 @@
+export const SET_AUTH_TOKEN: string = 'auth/SET_AUTH_TOKEN';
+export const SET_MY_USER: string = 'auth/SET_MY_USER';
+
+export const GET_AUTH_TOKEN: string = 'auth/GET_AUTH_TOKEN';
+export const GET_IS_AUTHORIZED = 'auth/GET_IS_AUTHORIZED';
+export const GET_MY_USER: string = 'auth/GET_MY_USER';
+export const GET_MY_USER_PROFILE_SRC: string = 'auth/GET_MY_USER_PROFILE_SRC';
+export const HAS_MY_USER_PROFILE_SRC: string = 'auth/HAS_PROFILE_SRC';
+
+export const DESTROY_AUTH_TOKEN: string = 'auth/DESTROY_AUTH_TOKEN';
+export const DESTROY_MY_USER: string = 'auth/DESTROY_MY_USER';
